80934e127fe7001220816e7adda58bea126a1a16
raylu <raylu@mixpanel.com>
2012-12-08 20:47:23 -0800

    add quiet mode to cron

--- db/cron.py
+++ db/cron.py
@@ -11,6 +11,13 @@ sys.path.append(pk_path)
 from db import api
 from db.parse_kills import parse_kills
 
+quiet = len(sys.argv) > 1 and sys.argv[1] == '-q'
+if quiet:
+	def log(*args):
+		pass
+else:
+	log = print
+
 log_path = path.join(pk_path, 'log')
 if not path.isdir(log_path):
 	os.mkdir(log_path)
@@ -19,17 +26,17 @@ timestamp = str(time.time())
 last_kill_id = None
 while True:
 	if last_kill_id is None:
-		print('querying for most recent kills')
+		log('querying for most recent kills')
 		xml_path = path.join(log_path, timestamp + '.xml')
 	else:
-		print('querying for before %s...' % last_kill_id)
+		log('querying for before %s...' % last_kill_id)
 		xml_path = path.join(log_path, '%s-%s.xml' % (timestamp, last_kill_id))
 	r = api.query(last_kill_id)
 	with open(xml_path, 'w') as f:
 		f.write(r.text)
-	print('\tinserting...')
+	log('\tinserting...')
 	last_kill_id = parse_kills(xml_path)
 	if not last_kill_id:
 		break
 	else:
-		print('\tdone')
+		log('\tdone')
